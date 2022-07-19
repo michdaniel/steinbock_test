4c29b385858ecacd712fbbda9d337b8ac5d13e6d
michdaniel <michelle.daniel@uzh.ch>
2022-07-19 09:02:45 +0000

    updated steinbock.sh

--- steinbock.sh
+++ steinbock.sh
@@ -1,15 +1,15 @@
+
 #!/bin/bash
 
 shopt -s expand_aliases
-alias steinbock="docker run -v /mnt/immucan_volume/processed_data/Panel_1/LUNG_cohort:/data -v /tmp/.X11-unix:/tmp/.X11-unix -v ~/.Xauthority:/home/steinbock/.Xauthority:ro -u $(id -u):$(id -g) -e DISPLAY ghcr.io/bodenmillergroup/steinbock:0.13.5"
+alias steinbock="docker run -v /mnt/bbdqbm/Data/20220707_Ican_p2_v07:/data -v /tmp/.X11-unix:/tmp/.X11-unix -v ~/.Xauthority:/home/steinbock/.Xauthority:ro -u $(id -u):$(id -g) -e DISPLAY ghcr.io/bodenmillergroup/steinbock:0.13.5"
 
-mkdir /mnt/immucan_volume/processed_data/Panel_1/LUNG_cohort/log/
+mkdir /mnt/bbdqbm/Data/20220707_Ican_p2_v07/log/
+steinbock preprocess imc panel --namecol Target
 
-steinbock preprocess imc panel --namecol Clean_Target
+steinbock preprocess imc images --hpf 50 &> /mnt/bbdqbm/Data/20220707_Ican_p2_v07/log/steinbock_img_log.txt
 
-steinbock preprocess imc images --hpf 50 &> /mnt/immucan_volume/processed_data/Panel_1/LUNG_cohort/log/steinbock_img_log.txt
-
-steinbock segment deepcell --minmax &> /mnt/immucan_volume/processed_data/Panel_1/LUNG_cohort/log/steinbock_segmentation_log.txt
+steinbock segment deepcell --minmax &> /mnt/bbdqbm/Data/20220707_Ican_p2_v07/log/steinbock_segmentation_log.txt
 
 steinbock measure intensities --masks masks -o intensities
 steinbock measure regionprops --masks masks -o regionprops
